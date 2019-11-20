43862368a49a3290cbe11a68aa8d3dabdec11332
nivan <nmorgan@designloud.com>
2019-11-20 03:18:37 -0500

    updates

--- src/Services/ConfigService/TestConfigRepository.php
+++ src/Services/ConfigService/TestConfigRepository.php
@@ -69,6 +69,8 @@ class TestConfigRepository implements ConfigRepository
     {
         if (session('myRoute') === 'clients') {
             return session('leftPath');
+        } else {
+            return config('file-manager.leftPath');
         }
     }
 
